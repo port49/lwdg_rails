ea43231170d29b54da9dbb7870257bdebb89ab1e
CLR <clr@port49.com>
2008-09-11 11:47:59 -0400

    -- switch to Basic auth --

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,36 +1,36 @@
-require 'htauth/digest'
+require 'htauth/passwd'
 
 class User
-  @@file_location = File.join( RAILS_ROOT, '.htdigest' )
+  @@file_location = File.join( RAILS_ROOT, '.htpasswd' )
   @@realm = 'LWDG File Manager'
   
   def self.find( arg )
     case arg
     when :all
-      HTAuth::DigestFile.open( @@file_location ).load_entries().collect{ |u| { :name => u.split( ":" )[0], :realm => u.split( ":" )[1] } }
+      HTAuth::PasswdFile.open( @@file_location ).load_entries().collect{ |u| { :name => u.split( ":" )[0], :realm => u.split( ":" )[1] } }
     when match( /\w+/ )
     end
   end
   
   def self.create( params )
     return false if User.sanitize_username( params[:name] ) == "admin"
-    file = HTAuth::DigestFile.open( @@file_location, HTAuth::File::ALTER )  
-    file.add User.sanitize_username( params[:name] ), @@realm, params[:password]
+    file = HTAuth::PasswdFile.open( @@file_location, HTAuth::File::ALTER )  
+    file.add User.sanitize_username( params[:name] ), params[:password]
     file.save!
     true
   end
   
   def self.update( params )
-    file = HTAuth::DigestFile.open( @@file_location, HTAuth::File::ALTER )  
-    file.update params[:name], @@realm, params[:password]
+    file = HTAuth::PasswdFile.open( @@file_location, HTAuth::File::ALTER )  
+    file.update params[:name], params[:password]
     file.save!
     true
   end
   
   def self.delete( params )
     return false if params[:name] == "admin"
-    file = HTAuth::DigestFile.open( @@file_location, HTAuth::File::ALTER )  
-    file.delete params[:name], @@realm
+    file = HTAuth::PasswdFile.open( @@file_location, HTAuth::File::ALTER )  
+    file.delete params[:name]
     file.save!
     true
   end
